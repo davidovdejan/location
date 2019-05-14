a2a8744cef0de8c56ada7bc32102e09dfb3e6875
davidovdejan <45712395+davidovdejan@users.noreply.github.com>
2019-05-14 15:42:36 +0200

    Uploading to cloud :)

---
+++ Location/src/main/java/com/dejan/location/model/Location.java
@@ -0,0 +1,48 @@
+package com.dejan.location.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity		//mapping to db table location
+public class Location {
+
+	//creating fields according to data base location
+	@Id
+	private int id;
+	private String code;
+	private String name;
+	private String type;
+
+	//generate getters and setters
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	//generate toString for printing object
+	@Override
+	public String toString() {
+		return "Location [id=" + id + ", code=" + code + ", name=" + name + ", type=" + type + "]";
+	}
+	
+	
+}
